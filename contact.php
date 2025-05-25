63cfd5e69e7a9a657708606af8ecd4ec8202804c
Alaguraja14 <alaguraja.mscmba@gmail.com>
2025-05-25 08:19:39 +0530

    cloudfare and whatsapp integration

--- contact.php
+++ contact.php
@@ -1,154 +1,160 @@
 ﻿<?php include('header.php') ?>
 
 <!--Page Title-->
-		<section class="page-title" style="background-image:url(images/background/12.jpg)">
-			<div class="auto-container">
-				<div class="clearfix pt-5">
-					<div class="pull-left pt-5">
+<section class="page-title" style="background-image:url(images/background/12.jpg)">
+	<div class="auto-container">
+		<div class="clearfix pt-5">
+			<div class="pull-left pt-5">
+				<h2>Contact Us</h2>
+			</div>
+		</div>
+	</div>
+</section>
+<!--End Page Title-->
+
+<!-- Contact Page Section -->
+<section class="contact-page-section">
+	<div class="auto-container">
+		<div class="row clearfix">
+
+			<!-- Column -->
+			<div class="form-column col-lg-8 col-md-12 col-sm-12">
+				<div class="inner-column">
+					<!-- Sec Title -->
+					<div class="sec-title">
 						<h2>Contact Us</h2>
+						<div class="separate"></div>
 					</div>
-				</div>
-			</div>
-		</section>
-		<!--End Page Title-->
 
-		<!-- Contact Page Section -->
-		<section class="contact-page-section">
-			<div class="auto-container">
-				<div class="row clearfix">
+					<!-- Contact Form -->
+					<div class="contact-form">
 
-					<!-- Column -->
-					<div class="form-column col-lg-8 col-md-12 col-sm-12">
-						<div class="inner-column">
-							<!-- Sec Title -->
-							<div class="sec-title">
-								<h2>Contact Us</h2>
-								<div class="separate"></div>
-							</div>
+						<!-- Contact Form -->
+						<form method="post" action="whatsapp.php" id="contact-form" autocomplete="off">
+							<div class="row clearfix">
 
-							<!-- Contact Form -->
-							<div class="contact-form">
+								<div class="col-lg-6 col-md-6 col-sm-12 form-group">
+									<input type="text" name="name" placeholder="Name" required="">
+								</div>
 
-								<!-- Contact Form -->
-								<form method="post" action="mail.php" id="contact-form" autocomplete="off">
-									<div class="row clearfix">
+								<div class="col-lg-6 col-md-6 col-sm-12 form-group">
+									<input type="email" name="email" placeholder="Email" required="">
+								</div>
 
-										<div class="col-lg-6 col-md-6 col-sm-12 form-group">
-											<input type="text" name="name" placeholder="Name" required="">
-										</div>
-										
-										<div class="col-lg-6 col-md-6 col-sm-12 form-group">
-											<input type="email" name="email" placeholder="Email" required="">
-										</div>
+								<div class="col-lg-6 col-md-6 col-sm-12 form-group">
+									<input type="number" name="mobile" placeholder="Mobile Number" required="">
+								</div>
 
-										<div class="col-lg-6 col-md-6 col-sm-12 form-group">
-											<input type="number" name="mobile" placeholder="Mobile Number" required="">
-										</div>
+								<div class="col-lg-6 col-md-6 col-sm-12 form-group">
+									<input type="text" name="subject" placeholder="Subject" required="">
+								</div>
 
-										<div class="col-lg-6 col-md-6 col-sm-12 form-group">
-											<input type="text" name="subject" placeholder="Subject" required="">
-										</div>
+								<div class="col-lg-12 col-md-12 col-sm-12 form-group">
+									<textarea class="darma" name="message" placeholder="Comment..."></textarea>
+								</div>
 
-										<div class="col-lg-12 col-md-12 col-sm-12 form-group">
-											<textarea class="darma" name="message" placeholder="Comment..."></textarea>
-										</div>
-
-										<div class="col-lg-12 col-md-12 col-sm-12 form-group">
-											<button class="theme-btn submit-btn" type="submit" name="submit">Send
-												Now</button>
-										</div>
-
+								<div class="row col-lg-12 col-md-12 col-sm-12 form-group">
+									<div class="col-md-6">
+										<div class="cf-turnstile" data-sitekey="0x4AAAAAABdSIAx-5vyXQcfs"></div>
 									</div>
-								</form>
+									<div class="col-md-6 d-flex justify-content-md-end justify-content-center">
+										<button class="theme-btn submit-btn" type="submit" name="submit">Send
+											Now</button>
+									</div>
+
+								</div>
 
 							</div>
-							<!-- End Contact Form -->
+						</form>
 
-						</div>
 					</div>
-
-					<!-- Column -->
-					<div class="info-column col-lg-4 col-md-12 col-sm-12">
-						<div class="inner-column">
-							<!-- Sec Title -->
-							<div class="sec-title">
-								<h2>Contact info</h2>
-								<div class="separate"></div>
-							</div>
-							<div class="text">
-								Welcome to New Fathima Timbers, where the timeless beauty of wood meets the artistry of craftsmanship. 
-								Established with a passion for excellence, we take pride in offering a curated selection of high-class imported woods and providing unparalleled woodworking services.
-							</div>
-
-							<ul class="contact-info">
-								<li>
-									<span class="icon flaticon-placeholder-2"></span>
-									<strong>Old Palpannai,</strong>
-									Thanjavur Road, Trichy - 8
-								</li>
-
-								<li>
-									<span class="icon flaticon-call"></span>
-									<strong>
-										<a href="tel:+919677503332">+91 9677503332</a></strong>
-										<a href="mailto:newfathimatimbers66@gmail.com">newfathimatimbers66@gmail.com</a>
-								</li>
-
-								<li>
-									<span class="icon flaticon-24-hours-2"></span>
-									<strong>WORKING HOURS</strong>
-									Mon - Sat: 08.30am to 10.00pm
-								</li>
-							</ul>
-
-						</div>
-					</div>
+					<!-- End Contact Form -->
 
 				</div>
 			</div>
-		</section>
-		<!-- End Contact Page Section -->
 
-		<!-- Contact Map Section -->
-		<section class="contact-map-section">
-			<div class="auto-container">
-				<div class="map-outer">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3919.240022746383!2d78.7608979!3d10.7929199!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baaf508d6b93e21%3A0xed0148e62ed60d23!2sNew%20fathima%20timbers!5e0!3m2!1sen!2sin!4v1701149903981!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-				</iframe>
-				</div>
-			</div>
-		</section>
-		<!-- End Map Section -->
-
-		<!-- Call To Action Three Section -->
-		<section class="call-to-action-section-three" style="background-image: url(images/background/11.jpg)">
-			<div class="auto-container">
-				<div class="row clearfix">
-
-					<!-- Title Column -->
-					<div class="title-column col-lg-8 col-md-12 col-sm-12">
-						<div class="inner-column">
-							<div class="experiance" style="color: white;">With over 3 years of experience</div>
-							<h2>Turning ideas into reality with the warmth of wood. Reach out for your bespoke woodworking solutions.</h2>
-						</div>
+			<!-- Column -->
+			<div class="info-column col-lg-4 col-md-12 col-sm-12">
+				<div class="inner-column">
+					<!-- Sec Title -->
+					<div class="sec-title">
+						<h2>Contact info</h2>
+						<div class="separate"></div>
+					</div>
+					<div class="text">
+						Welcome to New Fathima Timbers, where the timeless beauty of wood meets the artistry of craftsmanship.
+						Established with a passion for excellence, we take pride in offering a curated selection of high-class imported woods and providing unparalleled woodworking services.
 					</div>
 
-					<!-- Button Column -->
-					<div class="btn-column col-lg-4 col-md-12 col-sm-12">
-						<div class="inner-column">
-							<div class="number">
-								<a href="tel:+919677503332" style="color:white">+91 9677503332</a>
-							</div>
-							<a href="contact.php" class="theme-btn btn-style-three"><span class="txt">
-								Contact Us</span>
-							</a>
-						</div>
-					</div>
+					<ul class="contact-info">
+						<li>
+							<span class="icon flaticon-placeholder-2"></span>
+							<strong>Old Palpannai,</strong>
+							Thanjavur Road, Trichy - 8
+						</li>
+
+						<li>
+							<span class="icon flaticon-call"></span>
+							<strong>
+								<a href="tel:+919677503332">+91 9677503332</a></strong>
+							<a href="mailto:newfathimatimbers66@gmail.com">newfathimatimbers66@gmail.com</a>
+						</li>
+
+						<li>
+							<span class="icon flaticon-24-hours-2"></span>
+							<strong>WORKING HOURS</strong>
+							Mon - Sat: 08.30am to 10.00pm
+						</li>
+					</ul>
 
 				</div>
 			</div>
-		</section>
-		<!-- End Call To Action Three Section -->
 
-		
+		</div>
+	</div>
+</section>
+<!-- End Contact Page Section -->
+
+<!-- Contact Map Section -->
+<section class="contact-map-section">
+	<div class="auto-container">
+		<div class="map-outer">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3919.240022746383!2d78.7608979!3d10.7929199!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baaf508d6b93e21%3A0xed0148e62ed60d23!2sNew%20fathima%20timbers!5e0!3m2!1sen!2sin!4v1701149903981!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+			</iframe>
+		</div>
+	</div>
+</section>
+<!-- End Map Section -->
+
+<!-- Call To Action Three Section -->
+<section class="call-to-action-section-three" style="background-image: url(images/background/11.jpg)">
+	<div class="auto-container">
+		<div class="row clearfix">
+
+			<!-- Title Column -->
+			<div class="title-column col-lg-8 col-md-12 col-sm-12">
+				<div class="inner-column">
+					<div class="experiance" style="color: white;">With over 3 years of experience</div>
+					<h2>Turning ideas into reality with the warmth of wood. Reach out for your bespoke woodworking solutions.</h2>
+				</div>
+			</div>
+
+			<!-- Button Column -->
+			<div class="btn-column col-lg-4 col-md-12 col-sm-12">
+				<div class="inner-column">
+					<div class="number">
+						<a href="tel:+919677503332" style="color:white">+91 9677503332</a>
+					</div>
+					<a href="contact.php" class="theme-btn btn-style-three"><span class="txt">
+							Contact Us</span>
+					</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+<!-- End Call To Action Three Section -->
+
+
 <?php include('footer.php') ?>
